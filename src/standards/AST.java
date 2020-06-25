6aacb26dfb908dc935ded5bf874bf25417b44ede
ruchinamaratunga <raruchin@gmail.com>
2020-06-25 11:38:11 +0530

    ASTbuild() is done. Before starting standardizing

--- src/standards/AST.java
+++ src/standards/AST.java
@@ -1,18 +1,45 @@
 package standards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AST {
     private ASTNode rootnode;
+    private ArrayList<ArrayList<Object>> nodeDetails;
 
-    public AST(ASTNode rootnode){
+    public AST(ASTNode rootnode, ArrayList<ArrayList<Object>> nodeDetails) {
         this.rootnode = rootnode;
+        this.nodeDetails = nodeDetails;
+    }
+
+    public void ASTBuild() {
+        this.nodebuild(this.rootnode, this.nodeDetails);
     }
 
     public void printAST() {
-        
+
     }
 
     public void standardize() {
-        
+
+    }
+
+    private void nodebuild(ASTNode node, List<ArrayList<Object>> list) {
+        ArrayList<Integer> childlines = new ArrayList<Integer>();
+        System.out.println(node.getType());
+        for (int i = 0; i < list.size(); i++) {
+            if ((long) list.get(i).get(1) == node.getCount()+1) {
+                ASTNode newNode = new ASTNode((String) list.get(i).get(0), new ArrayList<ASTNode>(), node,
+                        (long) list.get(i).get(1), (int) list.get(0).get(2));
+                node.addChild(newNode);
+                childlines.add(newNode.getLinenumber());
+            }
+        }
+        childlines.add(list.size());
+        for (int j = 0; j < node.getChildren().size(); j++) {
+            this.nodebuild(node.getChildren().get(j), list.subList(childlines.get(j), childlines.get(j + 1)));
+        }
+        return;
     }
 }
 
--- src/standards/ASTNode.java
+++ src/standards/ASTNode.java
@@ -1,34 +1,63 @@
 package standards;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class ASTNode {
-    private NodeType type;
-    private List<ASTNode> children;
+    private String type;
+    private ArrayList<ASTNode> children;
     private ASTNode parent;
+    private long count;
+    private int linenumber;
 
+    public ASTNode(String type, ArrayList<ASTNode> children, ASTNode parent, long count, int i) {
+        this.type = type;
+        this.children = children;
+        this.parent = parent;
+        this.count = count;
+        this.linenumber = i;
+    }
 
-    public NodeType getType() {
+    public int getLinenumber() {
+        return linenumber;
+    }
+
+    public String getType() {
         return type;
+    }
+
+    public long getCount() {
+        return count;
     }
 
     public ASTNode getParent() {
         return parent;
     }
 
-    public List<ASTNode> getChildren() {
+    public ArrayList<ASTNode> getChildren() {
         return children;
     }
 
-    public void setChildren(List<ASTNode> children) {
+    public void setChildren(ArrayList<ASTNode> children) {
         this.children = children;
     }
 
-    public void setType(NodeType type) {
+    public void setType(String type) {
         this.type = type;
     }
 
     public void setParent(ASTNode parent) {
         this.parent = parent;
+    }
+    
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public void addChild(ASTNode child) {
+        this.children.add(child);
+    }
+    
+    public void setLinenumber(int linenumber) {
+        this.linenumber = linenumber;
     }
 }
